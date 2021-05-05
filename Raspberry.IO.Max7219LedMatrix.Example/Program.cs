ac2ef078c4c1794e13358925b1283369ee4dfd56
Michal Tichý <michal.tichy@riganti.cz>
2021-05-05 19:22:09 +0200

    Added sample

---
+++ Raspberry.IO.Max7219LedMatrix.Example/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Threading.Tasks;
+using Raspberry.IO.Max7219LedMatrix.Display;
+using Unosquare.RaspberryIO;
+
+namespace Raspberry.IO.Max7219LedMatrix.Example
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var display = new Max7219MatrixDisplay(Pi.Spi.Channel0, 4);
+
+            display.Init();
+
+            display.SetText("TEST");
+            display.UpdateScreen();
+            display.SetBrightness(5);
+
+            await Task.Delay(500);
+
+            display.TurnOff();
+        }
+    }
+}
