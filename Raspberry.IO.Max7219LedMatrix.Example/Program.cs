f55bdf8f518829700041d3c32c82695fe81a3c13
Michal Tichý <email@tichymichal.net>
2022-09-17 17:41:56 +0200

    updated sample + upgraded to net6

--- Raspberry.IO.Max7219LedMatrix.Example/Program.cs
+++ Raspberry.IO.Max7219LedMatrix.Example/Program.cs
@@ -1,7 +1,7 @@
-﻿using System;
-using System.Threading.Tasks;
+﻿using System.Threading.Tasks;
 using Raspberry.IO.Max7219LedMatrix.Display;
 using Unosquare.RaspberryIO;
+using Unosquare.WiringPi;
 
 namespace Raspberry.IO.Max7219LedMatrix.Example
 {
@@ -9,15 +9,30 @@ namespace Raspberry.IO.Max7219LedMatrix.Example
     {
         static async Task Main(string[] args)
         {
+            Pi.Init<BootstrapWiringPi>();
+
             var display = new Max7219MatrixDisplay(Pi.Spi.Channel0, 4);
 
             display.Init();
+            await Task.Delay(500);
 
-            display.SetText("TEST");
+            display.TurnOn();
+            await Task.Delay(500);
+
+            display.TurnOn();
+
+            display.ClearDisplay();
             display.UpdateScreen();
-            display.SetBrightness(5);
 
             await Task.Delay(500);
+            display.SetBrightness(5);
+
+            for (var i = 0; i < 9999; i++)
+            {
+                display.SetText(i.ToString().PadLeft(4, '0'));
+                display.UpdateScreen();
+                await Task.Delay(50);
+            }
 
             display.TurnOff();
         }
